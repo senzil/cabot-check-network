f7426f0cdeb9d7203a5c33eaa92083487a809172
Ladrusca <tomygrossi1998@gmail.com>
2021-07-17 11:42:34 -0300

    django.urls

--- cabot_check_network/views.py
+++ cabot_check_network/views.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.http import HttpResponseRedirect
 
 from cabot.cabotapp.models import StatusCheck
