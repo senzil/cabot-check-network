5a89f59a92794affa4c2e45f0110fb57c98407da
Ladrusca <tomygrossi1998@gmail.com>
2021-08-07 13:23:29 -0300

    dependencies fixed

--- cabot_check_network/migrations/0001_initial.py
+++ cabot_check_network/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('cabotapp', '0006_auto_20170821_1000'),
+        ('cabotapp', '0001_initial'),
     ]
 
     operations = [
